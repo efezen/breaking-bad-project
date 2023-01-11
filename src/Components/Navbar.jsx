c90d70117ddc2e3ac72a31300eec8312b75858d4
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-01-11 21:33:47 +0300

    finish

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../imgs/logo.png";
+function Navbar() {
+  return (
+    <header className="center">
+      <img src={logo} alt="Logo" />
+    </header>
+  );
+}
+
+export default Navbar;
